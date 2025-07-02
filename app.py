50e9e88f9dc30a4a8b85346c1ab811c6283e678b
annkaewstic <72075075+annkaewstic@users.noreply.github.com>
2025-07-02 13:55:00 +1000

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ THEME = {
 
 BASE_RAW = "https://raw.githubusercontent.com/annkaewstic/AdmisStudents/release/"
 CSV_URL   = BASE_RAW + quote("Список студентов для расчета контингента на 06.2025.csv")
-APPL_URL  = BASE_RAW + quote("2024 год.csv")
+APPL_URL  = BASE_RAW + quote("Итоги приемной комиссии за 2024 год.csv")
 
 PDF_FILES = [
     "Количество мест на обучение в 2024 году. Очная.pdf",
